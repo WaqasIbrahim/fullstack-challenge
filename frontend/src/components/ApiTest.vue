ccd004ae709a994e42bbb4d732337e436b18aa88
Waqas Ibrahim <waqas.abrahim@gmail.com>
2023-03-03 19:09:47 +0500

    add editorconfig, prettier config and cleanup frontend

--- frontend/src/components/ApiTest.vue
+++ frontend/src/components/ApiTest.vue
@@ -1,31 +1,27 @@
 <script>
 export default {
-  data: () => ({
-    apiResponse: null
-  }),
+    data: () => ({
+        apiResponse: null,
+    }),
 
-  created() {
-    this.fetchData()
-  },
+    created() {
+        this.fetchData();
+    },
 
-  methods: {
-    async fetchData() {
-      const url = 'http://localhost/'
-      this.apiResponse = await (await fetch(url)).json()
-    }
-  }
-}
+    methods: {
+        async fetchData() {
+            const url = 'http://localhost/';
+            this.apiResponse = await (await fetch(url)).json();
+        },
+    },
+};
 </script>
 
 <template>
-  <div v-if="!apiResponse">
-    Pinging the api...
-  </div>
+    <div v-if="!apiResponse">Pinging the api...</div>
 
-  <div v-if="apiResponse">
-    The api responded with: <br />
-    <code>
-    {{ apiResponse }}
-    </code>
-  </div>
+    <div v-if="apiResponse">
+        The api responded with: <br />
+        <code>{{ apiResponse }}</code>
+    </div>
 </template>
--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,9 +1,9 @@
 <script setup lang="ts">
-import ApiTest from "@/components/ApiTest.vue";
+import ApiTest from '@/components/ApiTest.vue';
 </script>
 
 <template>
-  <main>
-    <api-test></api-test>
-  </main>
+    <main>
+        <api-test></api-test>
+    </main>
 </template>
